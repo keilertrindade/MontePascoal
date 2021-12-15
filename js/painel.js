141bdb2c732131fa23716ab832da9bef8667ef93
keilertrindade <31934263+keilertrindade@users.noreply.github.com>
2021-12-15 17:00:14 -0300

    15/12/2021 [2]
    - Criação de arquivo painel.js para códigos javascript do painel (basicamente onclick dos botões). Nenhum código útil ainda feito.
    
    - Adição do carregamento dinâmico do nome e do número de atletas. No texto do painel.
    
    -  Até a metade do código de carregar o select box realizado. Ainda mostra 'Array' pois a variavel é um conjunto de arrays multidimensionais. Buscar forma de acessar os dados especificos do array no foreach.

---
+++ js/painel.js
@@ -0,0 +1,58 @@
+//var idequipe;
+
+function carregar(){
+	
+	document.getElementById("textobemvindo").innerHTML = "Texto";
+	
+}
+
+
+function sair() {
+alert("logoff");
+}
+
+
+function editarEquipe(){
+	
+	alert(idequipe);
+	
+	/*var nome = document.getElementById("name-f5dd").value;
+	var email = document.getElementById("email-f5dd").value;
+	var senha = document.getElementById("text-37d8").value;
+	var csenha = document.getElementById("text-37d9").value;
+	var cidade = document.getElementById("text-bc4d").value;
+	var estado = document.getElementById("text-8ec8").value;
+	
+	var fem30 = document.getElementById("checkbox-1dea").checked;
+	var fem40 = document.getElementById("checkbox-18e2").checked;
+	var fem50 = document.getElementById("checkbox-a07d").checked;
+	var mas35 = document.getElementById("checkbox-5669").checked;
+	var mas40 = document.getElementById("checkbox-0be0").checked;
+	var mas45 = document.getElementById("checkbox-8577").checked;
+	
+		if (!nome || !email || !senha || !csenha || !cidade || !estado){
+			alert("Por favor preencha todos os campos!");
+			return false;
+		}else if (fem30 == false && fem40 == false && fem50 == false && mas35 == false && mas40 == false && mas45 == false)
+		{
+			alert("Por favor selecione pelo menos uma categoria!");
+			return false;
+		}else if (csenha != senha){
+			alert("Senhas não são iguais. Por favor confira!");
+			return false;
+		}		
+		else {
+			alert("Tudo preenchido!"); //Adicionar ajax a partir daqui
+			return false; 
+		}
+	*/
+	
+}
+
+function cadastarAtleta(){
+	alert("cadastrarAtleta");
+}
+
+function editarAtleta(){
+	alert("editarAtleta");
+}
--- open.php
+++ open.php
@@ -3,7 +3,7 @@
 session_start();
 // as variáveis login e senha recebem os dados digitados na página anterior
 $login = $_POST['username'];
-$senha = $_POST['password'];//$_POST['password-a30d'];
+$senha = $_POST['password'];
 
 	$servername="localhost:3308";
 	$username="root";
@@ -18,6 +18,9 @@ $senha = $_POST['password'];//$_POST['password-a30d'];
 
 $sql = "SELECT * FROM `equipe`
 WHERE `email` = '$login'";
+
+$sqlatletas = "SELECT idequipe, nome FROM `equipe`";
+
 /* Logo abaixo temos um bloco com if e else, verificando se a variável $result foi
 bem sucedida, ou seja se ela estiver encontrado algum registro idêntico o seu valor
 será igual a 1, se não, se não tiver registros seu valor será 0. Dependendo do
@@ -26,22 +29,31 @@ do formulário inicial para que se possa tentar novamente realizar o login */
 $result = $conn->query($sql);
 //echo mysqli_num_rows($result);
 $row = mysqli_fetch_array($result);
+//print_r($row);
 //echo $row['senha'];
 
 if(mysqli_num_rows($result) == 1){
 	if (password_verify($senha,$row['senha'])){
 	
+		$result2 = $conn->query($sqlatletas);
+	    $atletasequipe = $result2->fetch_all(MYSQLI_ASSOC);
+	
 		$_SESSION['login'] = $login;
 		$_SESSION['senha'] = $senha;
 		$_SESSION['id'] = $row['idequipe'];
+		$_SESSION['nome'] = $row[3];
+		$_SESSION['atletas'] = $row[6];
+		
+		$_SESSION['list'] = $atletasequipe;
+		
 		mysqli_close($conn);
+		//print_r($_SESSION['list']);
+		//var_dump($_SESSION['list']);
 		echo "<script>
 				alert('Bem Vindo!');
 				window.location.href='painel.php';
 			 </script>";
-		
 	}
-
 }
 
 else{
